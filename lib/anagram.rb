46e9592e2294093c38dd08d84402036fad712fff
Juan Tenezaca <tenj34@gmail.com>
2018-08-05 16:15:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+   word_array.select do |word|
+     word.split("").sort == @word.split("").sort # splitting until we see the word in initilize ,,atch with the wowrd we split
+   end
+ end
+end
