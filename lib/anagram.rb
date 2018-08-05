bae535ec1769b981059062bb9601af19dfea187f
Juan Tenezaca <tenj34@gmail.com>
2018-08-05 16:08:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,10 +5,10 @@ class Anagram
   def initialize
     @word = word
   end
+
   def match(word_array)
    word_array.select do |word|
      word.split("").sort == @word.split("").sort
    end
  end
-
 end
